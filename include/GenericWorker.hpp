a1630346432b7412d6318ee90aa8738d00e22b73
SeungRyeol Lee <zizztux@gmail.com>
2020-03-22 22:31:29 +0900

    libcppbase: add missing header inclusion

--- include/GenericWorker.hpp
+++ include/GenericWorker.hpp
@@ -31,6 +31,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <string>
+
 #include <BlockingQueue.hpp>
 #include <WorkerBase.hpp>
 
--- include/PeriodicWorker.hpp
+++ include/PeriodicWorker.hpp
@@ -32,6 +32,7 @@
  */
 
 #include <chrono>
+#include <string>
 
 #include <WorkerBase.hpp>
 
